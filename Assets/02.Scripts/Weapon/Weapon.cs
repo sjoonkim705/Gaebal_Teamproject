7f444bf3740720a8403fdbe1e75d871d4d740f30
Jun Kim <sjkim705@gmail.com>
2024-02-01 12:48:57 +0900

    [김성준] 총알 적 향해 발사 구현

--- Assets/02.Scripts/Weapon/Weapon.cs
+++ Assets/02.Scripts/Weapon/Weapon.cs
@@ -20,12 +20,14 @@ public class Weapon : MonoBehaviour
     private float _timer;
     public Transform Target;
     public WeaponType WType;
+    Scanner _scanner;
+
 
     private void Awake()
     {
         // player = GetComponentInParent<Player>();
        _timer = 0;
-        if (WType == WeaponType.Kunai)
+        if (WType == WeaponType.Kunai || WType == WeaponType.Kunai_Upgrade)
         {
             CoolTime = 1.0f;
 
@@ -33,16 +35,23 @@ public class Weapon : MonoBehaviour
     }
     void Start()
     {
-        Scanner scanner = GetComponentInParent<Scanner>();
-        Target = scanner.nearestTarget;
+        if (WType == WeaponType.Kunai || WType == WeaponType.Kunai_Upgrade)
+        {
+            CoolTime = 1.0f;
+            _scanner = GetComponentInParent<Scanner>();
+            Target = _scanner.nearestTarget;
+        }
+
     }
 
   
     void Update()
     {
+        Target = _scanner.nearestTarget;
         _timer += Time.deltaTime;
-        if (_timer > CoolTime)
+        if (_timer > CoolTime && Target != null)
         {
+            Debug.Log($"{Target.transform.position.x} {Target.transform.position.y}");
             Fire();
             _timer = 0;
         }
@@ -51,11 +60,11 @@ public class Weapon : MonoBehaviour
     public void Fire()
     {
         // Debug.Log("Fire");
-        GameObject bullet = GameObject.Instantiate(Bullet_prefab);
+        GameObject bulletObject = GameObject.Instantiate(Bullet_prefab);
         //bullet.transform.SetParent(this.transform);
-        bullet.transform.position = transform.position;
-        //bullet.get
-        // bullet.dir = 
+        bulletObject.transform.position = transform.position;
+        Kunai_bullet kunai = bulletObject.GetComponent<Kunai_bullet>();
+        kunai.Target = Target;
 
     }
 }
