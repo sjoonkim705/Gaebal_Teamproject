b3c26de32077d550cd4cf6d2dcc895bb579780fb
yejin <yejin0195@gmail.com>
2024-02-01 10:04:04 +0900

    [민예진] Enemy 이동, Player를 따라가는 Enemy 스크립트 작성

--- Assets/02.Scripts/Enemy/Enemy.cs
+++ Assets/02.Scripts/Enemy/Enemy.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 1번 적 이동
+    // 2번 플레이어 팔로우
+    // 3번 무기로 죽음
+
+
+    public float Speed;
+    public int Health;
+
+    public GameObject target;
+    private Vector2 dir;
+
+    SpriteRenderer EnemySpriter;
+
     void Start()
     {
-        
+        EnemySpriter = GetComponent<SpriteRenderer>(); // 2번
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        EnemyMove();
+    }
+    void EnemyMove() // 1번
+    {
+        target = GameObject.Find("Player");
+
+        dir = target.transform.position - this.transform.position;
+        dir = dir.normalized;
+
+        transform.position += (Vector3)(dir * Speed) * Time.deltaTime;
+    }
+
+    void LateUpdate() // 2번
+    {
+        EnemySpriter.flipX = target.transform.position.x > this.transform.position.x;
     }
 }
