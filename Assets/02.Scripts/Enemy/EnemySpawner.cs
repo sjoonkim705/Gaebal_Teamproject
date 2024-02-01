45e69a5f5924313796cfb315e6aef87b480e9c16
yejin <yejin0195@gmail.com>
2024-02-01 12:49:45 +0900

    [민예진] Enemy 스폰 스크립트 생성

---
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public GameObject EnemySpawn;
+
+    public float Timer = 3f;
+    float _enemyrate;
+
+    public float MinTime = 0.5f;
+    public float MaxTime = 1.5f;
+
+    void Start()
+    {
+        RandomRete();
+    }
+
+    
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+
+        if (Timer < 0) 
+        {
+            RandomRete();
+            RandomSpawnTime();
+        }
+    }
+    void RandomSpawnTime() 
+    {
+        Timer = Random.Range(MinTime, MaxTime);
+    }
+    void RandomRete() 
+    {
+        
+
+        GameObject enemy;
+
+        _enemyrate = Random.Range(0, 100);
+
+        enemy = Instantiate(EnemyPrefab);
+
+        enemy.transform.position = EnemySpawn.transform.position;
+
+    }
+}
