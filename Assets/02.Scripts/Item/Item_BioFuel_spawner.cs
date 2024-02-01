1bf758b91c232efe9fa0874f89c102287fd56bef
공지수 <giroo27@gmail.com>
2024-02-01 12:48:03 +0900

    [공지수] 바이오 연료 스포너 추가 - 시작할때 랜덤 생성

--- Assets/02.Scripts/Item/Item_BioFuel_spawner.cs
+++ Assets/02.Scripts/Item/Item_BioFuel_spawner.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Item_BioFuel_spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject BioFuel_blue;
+    public GameObject BioFuel_green;
+    public GameObject BioFuel_yellow;
+    public int initialFuelCount = 999;
+    public int poolSize = 1000;
+
+    private List<GameObject> biofuelPool = new List<GameObject>();
+
+
+        void Start()
     {
+        initialFuelPool();
+        spawninitialFuel();
+    }
+
+    void initialFuelPool() 
+    {
+        for (int i = 0; i < poolSize; i++) 
+        {
+            GameObject biofuel = Instantiate(BioFuel_blue);
+            biofuel.SetActive(false);
+            biofuelPool.Add(biofuel);
+        }
         
+    }
+
+    void spawninitialFuel() 
+    {
+        for (int i = 0; i < initialFuelCount; i++)
+        {
+            GameObject bioFuel = biofuelPool[i];
+            Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), Random.Range(-10f, 10f), 0f);
+            bioFuel.transform.position = randomPosition;
+            bioFuel.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+  
         
     }
 }
