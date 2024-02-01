e37838338292cc8a1b61402c2bdd28317ae66667
Jun Kim <sjkim705@gmail.com>
2024-02-01 16:19:51 +0900

    [김성준] push/pull을 위한 commit

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -56,6 +56,7 @@ public class Player : MonoBehaviour
         {
             PlayerLevel++;
             LevelCount = 0;
+            Debug.Log($"LevelUp : Level = {PlayerLevel}");
         }
 
     }
