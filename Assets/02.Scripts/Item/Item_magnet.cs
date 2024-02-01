fa7f68cdd7424b251e0624ac587bd1887036ab0f
공지수 <giroo27@gmail.com>
2024-02-01 15:05:58 +0900

    [공지수] 아이템 마그넷 플레이어 따라다니면서 항상 y축으로 +1 이동

--- Assets/02.Scripts/Item/Item_magnet.cs
+++ Assets/02.Scripts/Item/Item_magnet.cs
@@ -4,31 +4,37 @@ using UnityEngine;
 
 public class Item_magnet : MonoBehaviour
 {
-    public float Speed;
-    private Vector2 _dir;
-
-
-
-
+    public float Speed = 10f;
+    public GameObject bioFuel;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Debug.Log($"아이템 생성");
-            comeBioFuel();
+            Debug.Log($"Alpha1 키 입력 감지");
         }
 
+        // 플레이어를 따라오게끔 업데이트
+        FollowPlayer();
     }
 
 
-    private void comeBioFuel()
+    private void FollowPlayer()
     {
+  
+                GameObject playerObject = GameObject.Find("Player");
+                if (playerObject != null)
+                {
+                    Vector3 pos = playerObject.transform.position;
+                    Vector3 follow = new Vector3(pos.x, pos.y + 1f, pos.z);
 
-        GameObject biofuel = GameObject.Find("BioFuel");
-        _dir = biofuel.transform.position - this.transform.position;
-        _dir.Normalize();
-        Vector2 newPosition = transform.position + (Vector3)(_dir * Speed) * Time.deltaTime;
-        transform.position = newPosition;
+                    // 플레이어 방향으로 이동
+                    Vector3 playerDirection = (follow - transform.position).normalized;
+                    transform.position += playerDirection * Speed * Time.deltaTime;
+                }
+                else
+                {
+                    Debug.LogWarning("플레이어를 찾을 수 없음");
+                }
     }
 }
