0251c79ef3db8a12bca76a0515188bf86e0feae6
yejin <yejin0195@gmail.com>
2024-02-02 14:59:35 +0900

    [민예진] 에너미 프리펩 변경 / 에너미 일정 시간 지나면 많이 나오게 코드 추가

--- Assets/02.Scripts/Enemy/EnemySpawner.cs
+++ Assets/02.Scripts/Enemy/EnemySpawner.cs
@@ -4,28 +4,67 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    // 에너미 리스폰 될 위치 담은 배열
     public Transform[] spawnPoint;
-
+    // 에너미 리스폰 간격 제어 타이머
     float _timer;
+    
+    // 경과 시간 측정
+    public float timeElapsed;
+    // 초기 에너미 리스폰 간격
+    float initalSpawnInterval = 0.5f;
+    // 현재 에너미 리스폰 간격
+    float currentSpawnInterval;
+    // 증가시킬 에너미 리스폰 간격
+    float doubleSpawnInterval = 0.2f;
+    // 증가시킨 에너미 리스포 기간
+    float doublingDuration = 10f;
 
     private void Awake()
     {
+        // 자식 오브젝트 중에 있는 모든 Transform 가져와서 배열에 저장
         spawnPoint = GetComponentsInChildren<Transform>(); // 중간에 s 확인 잘하기
+
+        // 초기 에너미 리스폰 간격 설정
+        currentSpawnInterval = initalSpawnInterval;
     }
 
     private void Update()
     {
+        // 타이머 업데이트
         _timer += Time.deltaTime;
+        // 경과 시간 업데이트
+        timeElapsed += Time.deltaTime;
 
-        if (_timer > 0.5f) 
+        // 일정 간격마다
+        if (_timer > currentSpawnInterval) 
         {
+            // Spawn 메서드 호출
             Spawn();
+            // 타이머 초기화
             _timer = 0;
         }
+        // 100초가 지나면
+        if (timeElapsed >= 100f) 
+        {
+            // 에너미 리스폰 간격 2배
+            currentSpawnInterval = doubleSpawnInterval;
+            
+        }
+        // 30초 이후에는
+        if (timeElapsed >= 30f)
+        {
+            // 다시 초기 간격으로 변경
+            currentSpawnInterval = initalSpawnInterval;
+            timeElapsed = 0f;
+        }
     }
+    // 에너미 리스폰 메서드
     void Spawn() 
     {
+        // 게임 매니저에서 오브젝트 풀에서 에너미를 가져와 생성
         GameObject enemy = GameManager.Instance.pool.Get(Random.Range(0, 1));
+        // 랜덤한 위치에 에너미 배치
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
