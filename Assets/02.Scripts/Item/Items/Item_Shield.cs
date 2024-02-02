c809fe3369f4213b34943003aedf482c71b00557
공지수 <giroo27@gmail.com>
2024-02-02 16:05:14 +0900

    [공지수] 쉴드 생성 / 쉴드 VFX 적용 / 쉴드 이미지 프리펩 삭제

--- Assets/02.Scripts/Item/Items/Item_Shield.cs
+++ Assets/02.Scripts/Item/Items/Item_Shield.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Item_Shield : MonoBehaviour
 {
+    public Animator ShieldAnimator;
 
-
+    private void Awake()
+    {
+        ShieldAnimator = this.gameObject.GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -14,7 +18,15 @@ public class Item_Shield : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+            enemy.Health--;
+            if (enemy.Health <= 0)
+            {
+                enemy.gameObject.SetActive(false);
+            }
+        }
     }
 
 }
