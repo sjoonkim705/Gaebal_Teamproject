39fc02f117402e7eaee0d827bb8cc306b66edc18
공지수 <giroo27@gmail.com>
2024-02-05 12:51:04 +0900

    [공지수] 운동화 아이템 - 이동속도 10%씩 증가 최대속도 증가 제한

---
+++ Assets/02.Scripts/Item/Items/Item_shoes.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Item_shoes : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player player = GetComponent<Player>();
+        player.Speed *= 1.1f;
+        float maxSpeed = 10.0f; // 최대 이동 속도 설정
+        player.Speed = Mathf.Min(player.Speed, maxSpeed);
+    }
+}
