85e2ca57237756f168bf1a4b68ed5e0aa825b170
Jun Kim <sjkim705@gmail.com>
2024-02-05 14:42:48 +0900

    [김성준] 타일, 슬라이더 관련 에셋 추가, HUD 스크립트 생성

---
+++ Assets/02.Scripts/HUD.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HUD : MonoBehaviour
+{
+    public enum InfoType
+    {
+        Exp, Level, Kill, Time, Health
+    }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    private void Awake()
+    {
+       myText = GetComponent<Text>();
+       mySlider = GetComponent<Slider>();
+
+    }
+
+    private void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+
+                break;            
+            case InfoType.Level:
+                break;       
+            case InfoType.Kill:
+                break; 
+            case InfoType.Time:
+                break; 
+            case InfoType.Health:
+                break;
+
+        }
+    }
+
+}
