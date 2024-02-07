9802bb9fa3b7af5d86566d28e0ae962b9dbf3996
yejin <yejin0195@gmail.com>
2024-02-07 19:14:23 +0900

    [민예진]

--- Assets/02.Scripts/UI/GameStart.cs
+++ Assets/02.Scripts/UI/GameStart.cs
@@ -9,7 +9,8 @@ public class GameStart : MonoBehaviour
 {
     public Sprite sprite1;
     public Sprite sprite2;
-    public float interval = 0.2f;
+    public float switchInterval = 0.3f;
+    private float _timer;
     private SpriteRenderer spriteRenderer;
 
 
@@ -18,23 +19,9 @@ public class GameStart : MonoBehaviour
     {
         GameManager.Instance.Stop();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        StartCoroutine(SwitchSprites());
 
     }
 
-    IEnumerator SwitchSprites()
-    {
-        while (true)
-        {
-            // sprite1을 보여주고 기다리기
-            spriteRenderer.sprite = sprite1;
-            yield return new WaitForSecondsRealtime(interval);
-
-            // sprite2를 보여주고 기다리기
-            spriteRenderer.sprite = sprite2;
-            yield return new WaitForSecondsRealtime(interval);
-        }
-    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
