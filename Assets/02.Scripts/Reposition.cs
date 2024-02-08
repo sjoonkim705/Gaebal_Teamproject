7cfdba021130e49154adb49b7bda8b897675346b
yejin <yejin0195@gmail.com>
2024-02-08 10:09:51 +0900

    [민예진] 풀 할고임

--- Assets/02.Scripts/Reposition.cs
+++ Assets/02.Scripts/Reposition.cs
@@ -14,12 +14,14 @@ public class Reposition : MonoBehaviour
     {
         coll = GetComponent<Collider2D>();
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
         {
             return;
         }
+        
         Vector3 playerPos = GameManager.Instance.player.transform.position;
         Vector3 myPos = transform.position;
 
@@ -66,12 +68,14 @@ public class Reposition : MonoBehaviour
                 }
                 break;
             case "Enemy":
-                if (coll.enabled) 
+                if (coll.enabled)
                 {
                     transform.Translate(playerDir * 20 + new Vector3(UnityEngine.Random.Range(-3f, 3f), UnityEngine.Random.Range(-3f, 3f), 0f));
                 }
                 break;
         }
+
+        
 
 
         
